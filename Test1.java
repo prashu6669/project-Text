5b1e61c54c24d9b7d415a1e67e059975ff3def21
prashu6669 <prasanthreddymarthala12@gmail.com>
2024-03-06 16:20:51 +0530

    first commit

---
+++ Test1.java
@@ -0,0 +1,22 @@
+public  class Test1{
+	public static void main(String[] args){
+	int a=20;
+	int b=30;
+	if (a<b){
+	System.out.println(a);
+
+
+	}
+	else{
+
+
+	System.out.println(b);
+}
+
+
+}
+
+
+
+
+}
